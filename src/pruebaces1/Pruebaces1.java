0c0a054f522761c7d791328c336ccaf6ac60fa3c
guadalupecortes030@gmail.com <>
2022-04-28 17:43:02 -0600

    prueba ces1

---
+++ src/pruebaces1/Pruebaces1.java
@@ -0,0 +1,15 @@
+
+package pruebaces1;
+
+
+public class Pruebaces1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("GUADALUPE CORTES PRUEBA GIT HUB");
+    }
+    
+}
